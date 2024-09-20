d624380fa6f6614b222925a61f77f806eb900061
mdabdullajobayer <mdabdullajovayer@gmail.com>
2024-09-20 20:59:52 +0600

    Admin Dashbord Summary Added

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Car;
+use App\Models\Rental;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function  index()
+    public function index()
     {
-        return view('Admin.Home');
+        $totalCars = Car::count();
+        $availableCars = Car::where('availability', 1)->count();
+        $totalRentals = Rental::count();
+        $totalEarnings = Rental::sum('total_cost');
+        return view('Admin.home', compact('totalCars', 'availableCars', 'totalRentals', 'totalEarnings'));
     }
 }
--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -4,17 +4,38 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Total Cars</h5>
+                                <p class="card-text">{{ $totalCars }}</p>
                             </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Available Cars</h5>
+                                <p class="card-text">{{ $availableCars }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Total Rentals</h5>
+                                <p class="card-text">{{ $totalRentals }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Total Earnings</h5>
+                                <p class="card-text">Taka: {{ number_format($totalEarnings, 2) }}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
